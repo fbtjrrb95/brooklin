815cac85fa866731faab88bca0c4f438dc47b551
fbtjrrb95 <58498606+fbtjrrb95@users.noreply.github.com>
2021-05-13 22:17:20 +0900

    Initial commit

---
+++ src/main/java/me/screw/brooklin/BrooklinApplication.java
@@ -0,0 +1,13 @@
+package me.screw.brooklin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrooklinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrooklinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/screw/brooklin/BrooklinApplicationTests.java
@@ -0,0 +1,13 @@
+package me.screw.brooklin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrooklinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
