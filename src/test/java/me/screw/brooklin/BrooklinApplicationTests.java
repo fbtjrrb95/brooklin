35128ba5049c6a4416797ba1a665c862b653831a
fbtjrrb95 <58498606+fbtjrrb95@users.noreply.github.com>
2021-05-17 23:21:48 +0900

    junit5 build gradle setting

--- src/test/java/me/screw/brooklin/BrooklinApplicationTests.java
+++ src/test/java/me/screw/brooklin/BrooklinApplicationTests.java
@@ -1,13 +1,16 @@
 package me.screw.brooklin;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
-class BrooklinApplicationTests {
+public class BrooklinApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
