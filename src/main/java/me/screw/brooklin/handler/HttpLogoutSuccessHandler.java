4b6e08f83b47b388dac9613090f8a6d7e9beb277
fbtjrrb95 <58498606+fbtjrrb95@users.noreply.github.com>
2021-05-24 22:00:51 +0900

    handler 구현

---
+++ src/main/java/me/screw/brooklin/handler/HttpLogoutSuccessHandler.java
@@ -0,0 +1,22 @@
+package me.screw.brooklin.handler;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HttpLogoutSuccessHandler implements LogoutSuccessHandler {
+    @Override
+    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        if(authentication ==null){
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        }else{
+            response.setStatus(HttpServletResponse.SC_OK);
+
+            // do something when logout
+        }
+    }
+}
