40cc1078b15580f2168be3f4208209eacdb14035
fbtjrrb95 <58498606+fbtjrrb95@users.noreply.github.com>
2021-05-26 12:28:36 +0900

    빈 등록되지 않는 이슈 수정

--- src/main/java/me/screw/brooklin/handler/AuthFailureHandler.java
+++ src/main/java/me/screw/brooklin/handler/AuthFailureHandler.java
@@ -2,6 +2,7 @@ package me.screw.brooklin.handler;
 
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -9,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+@Component
 public class AuthFailureHandler extends SimpleUrlAuthenticationFailureHandler {
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
