2846fa05fea2f6f98804e4df9f1dce4728994688
fbtjrrb95 <58498606+fbtjrrb95@users.noreply.github.com>
2021-05-27 22:26:36 +0900

    SecurityConfiguration 리팩토링

--- src/main/java/me/screw/brooklin/config/SecurityConfiguration.java
+++ src/main/java/me/screw/brooklin/config/SecurityConfiguration.java
@@ -5,14 +5,12 @@ import me.screw.brooklin.handler.AuthSuccessHandler;
 import me.screw.brooklin.handler.HttpLogoutSuccessHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -42,35 +40,24 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity
+                .csrf().disable()
+                .exceptionHandling()
+                    .authenticationEntryPoint(authenticationEntryPoint);
 
-        // Authentication Entry Point 설정
-        httpSecurity.exceptionHandling().authenticationEntryPoint(authenticationEntryPoint);
-
-        httpSecurity.csrf().disable();
-        httpSecurity.formLogin()
-                .permitAll()
-                .loginProcessingUrl("/login")
-                .usernameParameter("USERNAME")
-                .passwordParameter("PASSWORD")
-                .successHandler(authSuccessHandler)
-                .failureHandler(authFailureHandler)
-                .and()
-            .logout()
-                .permitAll()
-                .logoutRequestMatcher(new AntPathRequestMatcher("logout"))
-                .logoutSuccessHandler(logoutSuccessHandler)
-                .and()
-            .sessionManagement()
-                .maximumSessions(1)
-            ;
-
-        // "v1/feed" url만 authentication 없이 접근 허용
-        // 나머지는 authenticaion 필요
-
-        httpSecurity.authorizeRequests()
-                .antMatchers(HttpMethod.GET,"v1/feed").permitAll()
-                .anyRequest().authenticated()
-                ;
+        httpSecurity
+                .formLogin()
+                    .loginProcessingUrl("/auth/login")
+                    .usernameParameter("username")
+                    .passwordParameter("password")
+                    .successHandler(authSuccessHandler)
+                    .failureHandler(authFailureHandler)
+                    .and()
+                .logout()
+                    .logoutUrl("/auth/logout")
+                    .logoutSuccessHandler(logoutSuccessHandler)
+                    .and()
+                .authorizeRequests().anyRequest().authenticated();
     }
 
 }
