375cbb96b8f3ab2a45429b2d8a446c080263e615
fbtjrrb95 <58498606+fbtjrrb95@users.noreply.github.com>
2021-06-03 20:28:44 +0900

    test code (코드, 디비 드라이버) 추가

--- src/main/java/me/screw/brooklin/service/AccountService.java
+++ src/main/java/me/screw/brooklin/service/AccountService.java
@@ -2,14 +2,13 @@ package me.screw.brooklin.service;
 
 import me.screw.brooklin.domain.Account;
 import me.screw.brooklin.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import javax.persistence.PersistenceContext;
 
 @Service
 public class AccountService {
 
-    @PersistenceContext
+    @Autowired
     AccountRepository accountRepository;
 
     public void saveAccount(Account account){
