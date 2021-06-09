a700348898f77d48858fe36cedaccce2126dec0d
fbtjrrb95 <58498606+fbtjrrb95@users.noreply.github.com>
2021-06-09 19:52:08 +0900

    유저 등록 테스트 코드 정상작동하도록 수정 (RegisterRequest no args constructor 추가, request body 채워서 post 날리기)

--- src/main/java/me/screw/brooklin/request/RegisterRequest.java
+++ src/main/java/me/screw/brooklin/request/RegisterRequest.java
@@ -1,9 +1,16 @@
 package me.screw.brooklin.request;
 
+
+
 public class RegisterRequest {
     private String accountId;
     private String accountPassword;
 
+    // TODO: 이거 왜 NoArgsConstructor가 필요한거지?
+    // 이거랑 databind error가 나는걸까?
+    public RegisterRequest() {
+        super();
+    }
     public RegisterRequest(String accountId, String accountPassword) {
         this.accountId = accountId;
         this.accountPassword = accountPassword;
--- src/test/java/me/screw/brooklin/controller/registerControllerTest.java
+++ src/test/java/me/screw/brooklin/controller/registerControllerTest.java
@@ -1,10 +1,13 @@
 package me.screw.brooklin.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import me.screw.brooklin.request.RegisterRequest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -19,9 +22,16 @@ public class registerControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @Test
     public void registerMember() throws Exception {
-        mockMvc.perform(post("/register"))
+        String content = objectMapper.writeValueAsString(new RegisterRequest("seokkyu", "1234"));
+        mockMvc.perform(post("/register")
+                .content(content)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
         ;
     }
