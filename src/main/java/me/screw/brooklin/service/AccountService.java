3bc7efded8a533d919e30f3bae90c6e2017adfa7
fbtjrrb95 <58498606+fbtjrrb95@users.noreply.github.com>
2021-06-11 23:50:11 +0900

    accountService userdetailsService 구현하는 로직 추가

--- src/main/java/me/screw/brooklin/service/AccountService.java
+++ src/main/java/me/screw/brooklin/service/AccountService.java
@@ -3,15 +3,23 @@ package me.screw.brooklin.service;
 import me.screw.brooklin.domain.Account;
 import me.screw.brooklin.repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class AccountService {
+public class AccountService implements UserDetailsService {
 
     @Autowired
     AccountRepository accountRepository;
 
     public void saveAccount(Account account){
         accountRepository.save(account);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return accountRepository.findByUsername(username);
     }
 }
