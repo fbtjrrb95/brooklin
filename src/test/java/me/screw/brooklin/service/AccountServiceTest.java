3ee4165fd9d6d035741066922ae0cafdc1dedbad
fbtjrrb95 <58498606+fbtjrrb95@users.noreply.github.com>
2021-06-15 23:21:07 +0900

    accountService test 파일 생성

---
+++ src/test/java/me/screw/brooklin/service/AccountServiceTest.java
@@ -0,0 +1,16 @@
+package me.screw.brooklin.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AccountServiceTest {
+
+    @Test
+    public void saveAccount() {
+    }
+
+    @Test
+    public void loadUserByUsername() {
+    }
+}
