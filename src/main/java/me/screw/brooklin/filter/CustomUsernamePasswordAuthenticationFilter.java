da9bd092c90fb9a5abb1dfe5f3a09a4c768f3426
fbtjrrb95 <58498606+fbtjrrb95@users.noreply.github.com>
2021-06-21 23:20:43 +0900

    CustomUsernamePasswordAuthenticationFilter 구현#1

---
+++ src/main/java/me/screw/brooklin/filter/CustomUsernamePasswordAuthenticationFilter.java
@@ -0,0 +1,31 @@
+package me.screw.brooklin.filter;
+
+// https://johnmarc.tistory.com/74
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CustomUsernamePasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        return super.attemptAuthentication(request, response);
+    }
+
+    @Override
+    protected String obtainPassword(HttpServletRequest request) {
+        return super.obtainPassword(request);
+    }
+
+    @Override
+    protected String obtainUsername(HttpServletRequest request) {
+        return super.obtainUsername(request);
+    }
+
+    @Override
+    public void setPostOnly(boolean postOnly) {
+        super.setPostOnly(postOnly);
+    }
+}
