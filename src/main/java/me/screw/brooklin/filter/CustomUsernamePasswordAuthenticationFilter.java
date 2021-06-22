f73ebcc751d56fab5316dd705c483d920e41fa24
fbtjrrb95 <58498606+fbtjrrb95@users.noreply.github.com>
2021-06-23 00:08:31 +0900

    attemptAuthentication override 구현

--- src/main/java/me/screw/brooklin/filter/CustomUsernamePasswordAuthenticationFilter.java
+++ src/main/java/me/screw/brooklin/filter/CustomUsernamePasswordAuthenticationFilter.java
@@ -2,14 +2,20 @@ package me.screw.brooklin.filter;
 
 // https://johnmarc.tistory.com/74
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.stream.Collectors;
 
 
 public class CustomUsernamePasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
@@ -17,7 +23,35 @@ public class CustomUsernamePasswordAuthenticationFilter extends UsernamePassword
     private HashMap<String, String> jsonRequest;
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
-        return super.attemptAuthentication(request, response);
+        if(postOnly && !request.getMethod().equals("POST")){
+            throw new AuthenticationServiceException("Authentication method not supported: " + request.getMethod());
+        }
+
+        if(request.getHeader("Content-Type").equals(MediaType.APPLICATION_JSON)) {
+            ObjectMapper objectMapper = new ObjectMapper();
+            try {
+                this.jsonRequest = objectMapper.readValue(request.getReader().lines().collect(Collectors.joining()), new TypeReference<HashMap<String, String>>() {
+                });
+            } catch (IOException e) {
+                throw new AuthenticationServiceException("Request Content-Type(application/json) Parsing Error");
+            }
+        }
+
+        String username = obtainUsername(request);
+        String password = obtainPassword(request);
+
+        if(username == null){
+            username = "";
+        }
+
+        if(password == null){
+            password = "";
+        }
+
+        username = username.trim();
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(username,password);
+        setDetails(request, authToken);
+        return this.getAuthenticationManager().authenticate(authToken);
     }
 
     @Override
