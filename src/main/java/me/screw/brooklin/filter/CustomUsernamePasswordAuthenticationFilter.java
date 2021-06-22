ce2c193073fc2d922b0211d2ec57489cadee679d
fbtjrrb95 <58498606+fbtjrrb95@users.noreply.github.com>
2021-06-22 22:59:00 +0900

    obtainPassword, obtainUsername, setPostOnly override 구현하기

--- src/main/java/me/screw/brooklin/filter/CustomUsernamePasswordAuthenticationFilter.java
+++ src/main/java/me/screw/brooklin/filter/CustomUsernamePasswordAuthenticationFilter.java
@@ -1,14 +1,20 @@
 package me.screw.brooklin.filter;
 
 // https://johnmarc.tistory.com/74
+
+import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+
 
 public class CustomUsernamePasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    private boolean postOnly = true;
+    private HashMap<String, String> jsonRequest;
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
         return super.attemptAuthentication(request, response);
@@ -16,16 +22,24 @@ public class CustomUsernamePasswordAuthenticationFilter extends UsernamePassword
 
     @Override
     protected String obtainPassword(HttpServletRequest request) {
-        return super.obtainPassword(request);
+        String passwordParameter = super.getPasswordParameter();
+        if(request.getHeader("Content-Type").equals(MediaType.APPLICATION_JSON)){
+            return this.jsonRequest.get(passwordParameter);
+        }
+        return request.getParameter(passwordParameter);
     }
 
     @Override
     protected String obtainUsername(HttpServletRequest request) {
-        return super.obtainUsername(request);
+        String usernameParameter = super.getUsernameParameter();
+        if(request.getHeader("Content-Type").equals(MediaType.APPLICATION_JSON)){
+            return this.jsonRequest.get(usernameParameter);
+        }
+        return request.getParameter(usernameParameter);
     }
 
     @Override
     public void setPostOnly(boolean postOnly) {
-        super.setPostOnly(postOnly);
+        this.postOnly = postOnly;
     }
 }
